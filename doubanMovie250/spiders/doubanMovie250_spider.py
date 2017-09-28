785c246b900af7366875db5bdbde511698c0f8e8
mokusolo <liyundan3@gmail.com>
2017-09-28 10:41:44 +0800

    scrapy crawl doubanMovie250 -o doubanMovie250.csv, full 10 pages

--- doubanMovie250/spiders/doubanMovie250_spider.py
+++ doubanMovie250/spiders/doubanMovie250_spider.py
@@ -22,3 +22,7 @@ class DoubanMovie250Spider(scrapy.Spider):
             item['score'] = movie.xpath('.//div[@class="star"]/span[@class="rating_num"]/text()').extract()[0]
             item['quote'] = movie.xpath('.//p[@class="quote"]/span/text()').extract()
             yield item
+            next_url = response.xpath('//span[@class="next"]/a/@href').extract()
+            if next_url:
+                next_url = 'https://movie.douban.com/top250' + next_url[0]
+                yield scrapy.Request(next_url, headers=self.headers)
