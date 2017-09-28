5e38604f617c6c9a926604631dfc6d0431b31845
mokusolo <liyundan3@gmail.com>
2017-09-28 10:05:54 +0800

    scrapy crawl doubanMovie250 -o doubanMovie250.csv, only 1 page

---
+++ doubanMovie250/spiders/doubanMovie250_spider.py
@@ -0,0 +1,24 @@
+import scrapy
+from doubanMovie250.items import Doubanmovie250Item
+
+
+class DoubanMovie250Spider(scrapy.Spider):
+    name = 'doubanMovie250'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    }
+
+# 使用start_requests函数而不是start_urls = ['https://movie.douban.com/top250']，对初始URL的处理有更多权利，比如给初始URL增加请求头User-Agent
+    def start_requests(self):
+        url = 'https://movie.douban.com/top250'
+        yield scrapy.Request(url, headers=self.headers)
+
+    def parse(self, response):
+        item = Doubanmovie250Item()
+        movies = response.xpath('//ol[@class="grid_view"]/li')
+        for movie in movies:
+            item['ranking'] = movie.xpath('.//div[@class="pic"]/em/text()').extract()[0]
+            item['title'] = movie.xpath('.//div[@class="hd"]/a/span[1]/text()').extract()[0]
+            item['score'] = movie.xpath('.//div[@class="star"]/span[@class="rating_num"]/text()').extract()[0]
+            item['quote'] = movie.xpath('.//p[@class="quote"]/span/text()').extract()[0]
+            yield item
